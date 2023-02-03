afabd7c96e5b3fec5c727e3edce11986bebc1099
Usama Sarwar <UsamaSarwarOfficial@gmail.com>
2023-02-03 17:30:47 +0500

    Printing - Data Types - Loops

---
+++ printing.dart
@@ -0,0 +1,13 @@
+// Printing in Dart
+main() {
+  print('Hello World'); // String
+  print(25); // Integer
+  print(25.5); // Double
+  print(true); // Boolean
+  print(["Usama", "Sarwar"]); // List
+  print({"firstName": "Usama", "lastName": "Sarwar"}); // Map
+  print({"Pakistan", "India", "China"}); // Set
+}
+
+/// TODO: Printing Assignment
+/// Output the following text in the console: "You Name"
